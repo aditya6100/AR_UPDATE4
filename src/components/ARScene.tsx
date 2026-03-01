9bcf66c61d02b5b4d2cb25a4577b1a29c236ec99
Aditya Patil <adityabhanudas610@.com>
2026-03-02 02:49:26 +0530

    Align chevron arrows to point toward destination

--- src/components/ARScene.tsx
+++ src/components/ARScene.tsx
@@ -1065,9 +1065,8 @@ export default function ARScene({ floorData, activeSegment, pathSegments, startR
       const lookAt = pt.clone().add(tangent);
       group.lookAt(lookAt);
 
-      // Stand up and face forward along the path (reversed 180deg for correct alignment)
+      // Stand up and face forward along the path toward the destination
       chevron.rotateX(-Math.PI / 2); 
-      chevron.rotateY(Math.PI);
       
       group.add(chevron);
       group.userData.baseY = arrowY;
