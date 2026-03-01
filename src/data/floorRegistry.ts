47cc51788686a80d7fdd63a9f5c4ae565ccb46de
Aditya Patil <adityabhanudas610@.com>
2026-03-02 00:16:39 +0530

    Rename file.svg to vite.svg and fix 404

--- src/data/floorRegistry.ts
+++ src/data/floorRegistry.ts
@@ -18,11 +18,11 @@ export const floors: { id: string; number: number; label: string; marker?: { ima
     marker: { image: 'marker_f1.png', position: { x: 0.89, z: 3.65 } }
   },
   { 
-    id: 'f2', number: 2, label: '1st Floor',
+    id: 'f2', number: 2, label: '2nd Floor (CSE)',
     marker: { image: 'marker_f2.png', position: { x: 0.89, z: 3.65 } }
   },
   { 
-    id: 'f3', number: 3, label: '2nd Floor (CSE)',
+    id: 'f3', number: 3, label: 'Floor 3',
     marker: { image: 'marker.png', position: { x: 2.0, z: 1.6 } }
   },
   { 
@@ -62,7 +62,7 @@ export const verticalConnectors: {
     costPerFloor: 10,
     floorWaypoints: {
       f1: 'f1_wp_hall_stairs_bot',
-      f2: 'f2_wp_hall_stairs_bot',
+      f2: 'f2_wp_stairs_bot',
       f3: 'f3_wp_stairs_bot',
       f4: 'f4_wp_hall_stairs_bot',
       f5: 'f5_wp_hall_stairs_bot',
@@ -75,7 +75,7 @@ export const verticalConnectors: {
     costPerFloor: 10,
     floorWaypoints: {
       f1: 'f1_wp_hall_stairs_top',
-      f2: 'f2_wp_hall_stairs_top',
+      f2: 'f2_wp_stairs_top',
       f3: 'f3_wp_stairs_top',
       f4: 'f4_wp_hall_stairs_top',
       f5: 'f5_wp_hall_lab12',
